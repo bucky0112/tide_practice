edcdb13627dbc7594c2d3fe6bb2fc99f1b6bf3b0
bucky0112 <bucky0112@gmail.com>
2023-07-05 00:27:24 +0800

    Refactor main.rs to handle dynamic name parameters in the URL and return a personalized greeting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use tide::Request;
+
+#[derive(serde::Deserialize)]
+#[serde(default)]
+struct NameParams {
+    pub name: String,
+}
+
+impl Default for NameParams {
+    fn default() -> Self {
+        Self {
+            name: "world".to_string(),
+        }
+    }
+}
+
+async fn handle_name(req: Request<()>) -> tide::Result<String> {
+    let name = req
+        .url()
+        .query_pairs()
+        .find(|(key, _)| key == "name")
+        .map(|(_, value)| value);
+
+    Ok(format!("Hello, {}!", name.unwrap_or("world".into())))
+}
+#[tokio::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/").get(handle_name);
+    app.listen("0.0.0.0:8080").await?;
+    Ok(())
 }
